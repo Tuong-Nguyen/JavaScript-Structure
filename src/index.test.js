252e57507e81851f0d9ccb7c9ce55e576428bce5
Tuong-Nguyen <wallnguyen@gmail.com>
2017-04-07 15:06:05 +0700

    Add a test case

---
+++ src/index.test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by admin1 on 4/7/2017.
+ */
+import {expect} from 'chai';
+import {describe, it} from "mocha";
+
+
+describe("It is", () => {
+  it("passed", () => {
+    expect(true).to.equal(true);
+  });
+});
