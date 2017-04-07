8ae47d0032bc94c3a71493db502b62d45f2b598b
Tuong-Nguyen <wallnguyen@gmail.com>
2017-04-07 15:34:08 +0700

    Add a test case for index.html page

--- src/index.test.js
+++ src/index.test.js
@@ -3,10 +3,24 @@
  */
 import {expect} from 'chai';
 import {describe, it} from "mocha";
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 
 describe("It is", () => {
   it("passed", () => {
     expect(true).to.equal(true);
+  });
+});
+
+describe("index.html", () => {
+  it("says hello", (done) => {
+    let index = fs.readFileSync("./src/index.html", "utf-8");
+    jsdom.env(index, (error, window) => {
+      let h1 = window.document.getElementsByTagName("h1")[0];
+      expect(h1.innerHTML).to.equal("Hello World!");
+      done();
+      window.close();
+    });
   });
 });
