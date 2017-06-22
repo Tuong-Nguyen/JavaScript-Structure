c4bb91ec0272d09130d25259b432444ecaf59b1e
Le Ngoc Thao <lnthao@tma.com.vn>
2017-06-22 10:42:57 +0700

    Resolve additional test cases of ClassExtendsTest

--- es6-fundamental/src/class_tests/ClassExtendsTest.js
+++ es6-fundamental/src/class_tests/ClassExtendsTest.js
@@ -49,7 +49,7 @@ describe('classes can inherit from another', () => {
   });
 
   it('extend an `old style` "class", a function, still works', () => {
-    let A;
+    let A = function (){};
     class B extends A {}
 
     assert.equal(new B() instanceof A, true);
@@ -59,11 +59,11 @@ describe('classes can inherit from another', () => {
     class A {}
     class B extends A {}
     it('A is the prototype of B', () => {
-      const isIt = A.isPrototypeOf(null);
+      const isIt = A.isPrototypeOf(B);
       assert.equal(isIt, true);
     });
     it('A`s prototype is also B`s prototype', () => {
-      const proto = B;
+      const proto = B.prototype;
       // Remember: don't touch the assert!!! :)
       assert.equal(A.prototype.isPrototypeOf(proto), true);
     });
@@ -72,14 +72,14 @@ describe('classes can inherit from another', () => {
   describe('`extends` using an expression', () => {
     it('eg the inline assignment of the parent class', () => {
       let A;
-      class B extends (A = {}) {}
+      class B extends (A = class {}) {}
 
       assert.equal(new B() instanceof A, true);
     });
 
     it('or calling a function that returns the parent class', () => {
       const returnParent = (beNull) => beNull ? null : class {};
-      class B extends (returnParent) {}
+      class B extends (returnParent(1)) {}
 
       assert.equal(Object.getPrototypeOf(B.prototype), null);
     });
