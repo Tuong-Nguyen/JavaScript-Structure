dad4242382ae9a05cb2bef10cbcb30c1af0c860e
Le Ngoc Thao <lnthao@tma.com.vn>
2017-06-22 10:27:33 +0700

    Resolve ClassStaticTest

--- es6-fundamental/src/class_tests/ClassStaticTest.js
+++ es6-fundamental/src/class_tests/ClassStaticTest.js
@@ -1,5 +1,6 @@
 // 24: class - static keyword
 // To do: make all tests pass, leave the assert lines unchanged!
+import {assert} from 'chai';
 
 describe('inside a class you can use the `static` keyword', () => {
 
@@ -10,14 +11,14 @@ describe('inside a class you can use the `static` keyword', () => {
 
     it('a static method just has the prefix `static`', () => {
       class TestFactory {
-        makeTest() { return new UnitTest(); }
+        static makeTest() { return new UnitTest(); }
       }
 
       assert.ok(TestFactory.makeTest() instanceof UnitTest);
     });
 
     it('the method name can be dynamic/computed at runtime', () => {
-      const methodName = 'makeTest';
+      const methodName = 'createTest';
       class TestFactory {
         static [methodName]() { return new UnitTest(); }
       }
@@ -29,7 +30,7 @@ describe('inside a class you can use the `static` keyword', () => {
   describe('for accessors', () => {
     it('a getter name can be static, just prefix it with `static`', () => {
       class UnitTest {
-        get testType() { return 'unit'; }
+        static get testType() { return 'unit'; }
       }
 
       assert.equal(UnitTest.testType, 'unit');
@@ -38,7 +39,7 @@ describe('inside a class you can use the `static` keyword', () => {
     it('even a static getter name can be dynamic/computed at runtime', () => {
       const type = 'test' + 'Type';
       class IntegrationTest {
-        get type() { return 'integration'; }
+        static get [type]() { return 'integration'; }
       }
 
       assert.ok('testType' in IntegrationTest);
