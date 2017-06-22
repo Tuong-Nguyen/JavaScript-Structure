8f4f15b95d035aedce0e07583cf426a5e04ea5c0
Le Ngoc Thao <lnthao@tma.com.vn>
2017-06-22 11:13:43 +0700

    Resolve super is method part of ClassSuperTest

--- es6-fundamental/src/class_tests/ClassSuperTest.js
+++ es6-fundamental/src/class_tests/ClassSuperTest.js
@@ -1,24 +1,27 @@
 // 27: class - super inside a method
 // To do: make all tests pass, leave the assert lines unchanged!
 
+import {assert} from 'chai';
+
 describe('inside a class use `super` to access parent methods', () => {
 
   it('use of `super` without `extends` fails (already when transpiling)', () => {
-    class A {hasSuper() { return super; }}
+    class Parent { hasSuper() {return false;}}
+    class A extends Parent {hasSuper() { return super.hasSuper(); }}
 
     assert.equal(new A().hasSuper(), false);
   });
 
   it('`super` with `extends` calls the method of the given name of the parent class', () => {
     class A {hasSuper() { return true; }}
-    class B extends A {hasSuper() { return super.hasSuper; }}
+    class B extends A {hasSuper() { return super.hasSuper(); }}
 
     assert.equal(new B().hasSuper(), true);
   });
 
   it('when overridden a method does NOT automatically call its super method', () => {
     class A {hasSuper() { return true; }}
-    class B extends A {hasSuper() { return 'nothing'; }}
+    class B extends A {hasSuper() { }}
 
     assert.equal(new B().hasSuper(), void 0);
   });
@@ -28,7 +31,7 @@ describe('inside a class use `super` to access parent methods', () => {
     class B extends A {constructor() { super(); this.youAreSuper = true; } }
     class C extends B {
       iAmSuper() {
-        return this.iAmSuper();
+        return super.iAmSuper();
       }
     }
 
@@ -37,7 +40,7 @@ describe('inside a class use `super` to access parent methods', () => {
 
   it('accessing an undefined member of the parent class returns `undefined`', () => {
     class A {}
-    class B extends A {getMethod() { return super.constructor; }}
+    class B extends A {getMethod() { return super.undefinedMember; }}
 
     assert.equal(new B().getMethod(), void 0);
   });
