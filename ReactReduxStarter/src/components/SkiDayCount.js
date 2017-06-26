7c73f2c474730b1e23f9c9709fe32d7b42979510
Le Ngoc Thao <lnthao@tma.com.vn>
2017-06-26 14:11:46 +0700

    Use react-icons

--- ReactReduxStarter/src/components/SkiDayCount.js
+++ ReactReduxStarter/src/components/SkiDayCount.js
@@ -2,31 +2,59 @@
  * Created by lnthao on 6/23/2017.
  */
 import '../stylesheets/index.scss';
+import React from 'react';
+import Calendar from 'react-icons/lib/fa/calendar';
+import SnowFlake from 'react-icons/lib/ti/weather-snow';
+import Terrain from 'react-icons/lib/md/terrain';
 
 const percentToDecimal = (decimal) => {
   return ((decimal * 100) + '%');
-}
+};
 const calcGoalProgress = (total, goal) => {
   return percentToDecimal(total/goal);
-}
+};
 export const SkiDayCount = ({total, powder, backcountry, goal}) => (
-      <div className="ski-day-count">
-        <div className="total-days">
-          <span>{total}</span>
-          <span>days</span>
-        </div>
-        <div className="powder-days">
-          <span>{powder}</span>
-          <span>days</span>
-        </div>
-        <div className="backcountry-days">
-          <span>{backcountry}</span>
-          <span>days</span>
-        </div>
-        <div>
+  <div className="ski-day-count">
+    <div className="total-days">
+      <span>{total}</span>
+      <Calendar/>
+      <span>days</span>
+    </div>
+    <div className="powder-days">
+      <span>{powder}</span>
+      <SnowFlake/>
+      <span>days</span>
+    </div>
+    <div className="backcountry-days">
+      <span>{backcountry}</span>
+      <Terrain/>
+      <span>days</span>
+    </div>
+    <div>
           <span>
           {calcGoalProgress(total, goal)}
           </span>
-        </div>
-      </div>
-)
+    </div>
+  </div>
+);
+// export class SkiDayCount extends React.Component{
+//   render(){
+//     return (
+//       <div className="ski-day-count">
+//         <div className="total-days">
+//           <span>{this.props.total}</span>
+//           <span>days</span>
+//         </div>
+//         <div className="powder-days">
+//           <span>{this.props.powder}</span>
+//           <span>days</span>
+//         </div>
+//         <div className="backcountry-days">
+//           <span>{this.props.backcountry}</span>
+//           <span>days</span>
+//         </div>
+//         <div><span>{this.props.goal}</span></div>
+//       </div>
+//     );
+//   }
+// }
