f9a4cf8017255832bde887fb74060f1911f7871b
Le Ngoc Thao <lnthao@tma.com.vn>
2017-06-27 15:50:09 +0700

    Use refs in stateless component

--- ReactReduxStarter/src/components/AddDayForm.js
+++ ReactReduxStarter/src/components/AddDayForm.js
@@ -1,54 +1,50 @@
 /**
  * Created by lnthao on 6/27/2017.
  */
-import {Component, PropTypes} from 'react';
-export class AddDayForm extends Component {
-  constructor(props) {
-    super(props);
-    this.submit = this.submit.bind(this);
-  }
-  submit(e) {
+import {PropTypes} from 'react';
+export const AddDayForm = ({resort, date, powder, backcountry}) => {
+  let _resort, _date, _powder, _backcountry;
+  const submit = (e) => {
     e.preventDefault();
-    console.log('resort', this.refs.resort.value);
-    console.log('date', this.refs.date.value);
-    console.log('powder', this.refs.powder.checked);
-    console.log('backcountry', this.refs.backcountry.checked);
+    console.log('resort', _resort.value);
+    console.log('date', _date.value);
+    console.log('powder', _powder.checked);
+    console.log('backcountry', _backcountry.checked);
   }
-  render() {
-    const {resort, date, powder, backcountry} = this.props;
-    return (
-      <form onSubmit={this.submit} className="add-day">
-        <label htmlFor="resort">Resort Name</label>
-        <input id="resort"
-               type="text"
-               defaultValue={resort}
-               ref="resort"
-               required/>
-        <label htmlFor="date">Date</label>
-        <input id="date"
-               type="text"
-               defaultValue={date}
-               ref="date"
-               required/>
-        <div>
-          <input id="powder"
-                 type="checkbox"
-                 defaultChecked={powder}
-                 ref="powder"/>
-          <label htmlFor="powder">Powder Day</label>
-        </div>
-        <div>
-          <input id="backcountry"
-                 type="checkbox"
-                 defaultChecked={backcountry}
-                 ref="backcountry"/>
-          <label htmlFor="backcountry">Backcountry Day</label>
-        </div>
-        <button>Add Day</button>
-      </form>
-    );
-  }
-}
+
+  return (
+    <form onSubmit={submit} className="add-day">
+      <label htmlFor="resort">Resort Name</label>
+      <input id="resort"
+             type="text"
+             defaultValue={resort}
+             ref={input => _resort = input}
+             required/>
+      <label htmlFor="date">Date</label>
+      <input id="date"
+             type="text"
+             defaultValue={date}
+             ref={input => _date = input}
+             required/>
+      <div>
+        <input id="powder"
+               type="checkbox"
+               defaultChecked={powder}
+               ref={input => _powder = input}/>
+        <label htmlFor="powder">Powder Day</label>
+      </div>
+      <div>
+        <input id="backcountry"
+               type="checkbox"
+               defaultChecked={backcountry}
+               ref={input => _backcountry = input}/>
+        <label htmlFor="backcountry">Backcountry Day</label>
+      </div>
+      <button>Add Day</button>
+    </form>
+  );
+};
+
 AddDayForm.defaultProps = {
   resort: "Kirkwood",
   date: "2017-06-30",
