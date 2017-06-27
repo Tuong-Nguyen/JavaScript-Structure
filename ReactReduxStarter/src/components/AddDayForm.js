f75e70224a7697b05478091c8f93f3c255ff6c30
Le Ngoc Thao <lnthao@tma.com.vn>
2017-06-27 15:42:34 +0700

    Use refs in form component to load properties to form controls

--- ReactReduxStarter/src/components/AddDayForm.js
+++ ReactReduxStarter/src/components/AddDayForm.js
@@ -3,34 +3,48 @@
  */
 import {Component, PropTypes} from 'react';
 export class AddDayForm extends Component {
+  constructor(props) {
+    super(props);
+    this.submit = this.submit.bind(this);
+  }
+  submit(e) {
+    e.preventDefault();
+    console.log('resort', this.refs.resort.value);
+    console.log('date', this.refs.date.value);
+    console.log('powder', this.refs.powder.checked);
+    console.log('backcountry', this.refs.backcountry.checked);
+  }
   render() {
     const {resort, date, powder, backcountry} = this.props;
     return (
-      <form className="add-day">
+      <form onSubmit={this.submit} className="add-day">
         <label htmlFor="resort">Resort Name</label>
         <input id="resort"
                type="text"
                defaultValue={resort}
+               ref="resort"
                required/>
         <label htmlFor="date">Date</label>
         <input id="date"
                type="text"
                defaultValue={date}
+               ref="date"
                required/>
         <div>
           <input id="powder"
                  type="checkbox"
                  defaultChecked={powder}
-                 required/>
+                 ref="powder"/>
           <label htmlFor="powder">Powder Day</label>
         </div>
         <div>
           <input id="backcountry"
                  type="checkbox"
                  defaultChecked={backcountry}
-                 required/>
+                 ref="backcountry"/>
           <label htmlFor="backcountry">Backcountry Day</label>
         </div>
+        <button>Add Day</button>
       </form>
     );
   }
@@ -40,10 +54,10 @@ AddDayForm.defaultProps = {
   date: "2017-06-30",
   powder: true,
   backcountry: false
-}
+};
 AddDayForm.propTypes = {
   resort: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
   powder: PropTypes.bool.isRequired,
-  backcountry: PropTypes.bool.isRequired,
+  backcountry: PropTypes.bool.isRequired
 };
