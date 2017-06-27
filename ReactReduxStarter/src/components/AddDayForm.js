b54c7444fb7e8bac1a4c1e8db8d755de85166828
Le Ngoc Thao <lnthao@tma.com.vn>
2017-06-27 15:14:35 +0700

    Add a form to component

--- ReactReduxStarter/src/components/AddDayForm.js
+++ ReactReduxStarter/src/components/AddDayForm.js
@@ -1,6 +1,49 @@
 /**
  * Created by lnthao on 6/27/2017.
  */
-export const AddDayForm = () => (
-  <h1>Add a Day</h1>
-)
+import {Component, PropTypes} from 'react';
+export class AddDayForm extends Component {
+  render() {
+    const {resort, date, powder, backcountry} = this.props;
+    return (
+      <form className="add-day">
+        <label htmlFor="resort">Resort Name</label>
+        <input id="resort"
+               type="text"
+               defaultValue={resort}
+               required/>
+        <label htmlFor="date">Date</label>
+        <input id="date"
+               type="text"
+               defaultValue={date}
+               required/>
+        <div>
+          <input id="powder"
+                 type="checkbox"
+                 defaultChecked={powder}
+                 required/>
+          <label htmlFor="powder">Powder Day</label>
+        </div>
+        <div>
+          <input id="backcountry"
+                 type="checkbox"
+                 defaultChecked={backcountry}
+                 required/>
+          <label htmlFor="backcountry">Backcountry Day</label>
+        </div>
+      </form>
+    );
+  }
+}
+AddDayForm.defaultProps = {
+  resort: "Kirkwood",
+  date: "2017-06-30",
+  powder: true,
+  backcountry: false
+}
+AddDayForm.propTypes = {
+  resort: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  powder: PropTypes.bool.isRequired,
+  backcountry: PropTypes.bool.isRequired,
+};
