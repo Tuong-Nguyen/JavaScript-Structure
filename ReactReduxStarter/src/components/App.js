6288a908a2175317571753f52e90bacd7324a161
Le Ngoc Thao <lnthao@tma.com.vn>
2017-06-29 11:44:04 +0700

    Add header component that contains links

--- ReactReduxStarter/src/components/App.js
+++ ReactReduxStarter/src/components/App.js
@@ -2,12 +2,13 @@
  * Created by lnthao on 6/29/2017.
  */
 import React, {PropTypes} from 'react';
+import Header from './common/Header';
 
 class App extends React.Component{
   render() {
     return (
       <div className="container-fluild">
-        <p>Header here...</p>
+        <Header/>
         {this.props.children}
       </div>
     );
