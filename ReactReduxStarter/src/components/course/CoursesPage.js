1af6fc41111193dcca1cd996ac5c8a822bba520e
Le Ngoc Thao <lnthao@tma.com.vn>
2017-06-30 14:35:09 +0700

    Add courses page and its route

---
+++ ReactReduxStarter/src/components/course/CoursesPage.js
@@ -0,0 +1,16 @@
+/**
+ * Created by lnthao on 6/30/2017.
+ */
+import React, {PropTypes} from 'react';
+
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
