1a97bca80b5031a54b4d1f8b4c21362a2861a63b
Le Ngoc Thao <lnthao@tma.com.vn>
2017-07-05 13:47:54 +0700

    Move action type to separate file

--- ReactReduxStarter/src/actions/courseActions.js
+++ ReactReduxStarter/src/actions/courseActions.js
@@ -1,7 +1,9 @@
 /**
  * Created by lnthao on 7/4/2017.
  */
+import * as types from './actionTypes';
+
 export function createCourse(course){
   console.log('Action - createCourse');
-  return {type: 'CREATE_COURSE', course};
+  return {type: types.CREATE_COURSE, course};
 }
--- ReactReduxStarter/src/reducers/courseReducer.js
+++ ReactReduxStarter/src/reducers/courseReducer.js
@@ -1,10 +1,11 @@
 /**
  * Created by lnthao on 7/4/2017.
  */
+import * as types from '../actions/actionTypes';
 export default function courseReducer(state = [], action){
   console.log('Reducer - courseReducer');
   switch (action.type) {
-    case 'CREATE_COURSE':
+    case types.CREATE_COURSE:
       // state.push(action.course);
       // return state;
       return [...state,
