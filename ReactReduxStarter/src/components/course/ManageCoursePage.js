22b153b8ca2e10ad70743a63b434bb01e2397523
Le Ngoc Thao <lnthao@tma.com.vn>
2017-07-05 17:42:15 +0700

    Redirect via React router context

--- ReactReduxStarter/src/components/course/ManageCoursePage.js
+++ ReactReduxStarter/src/components/course/ManageCoursePage.js
@@ -28,6 +28,7 @@ class ManageCoursePage extends React.Component {
   saveCourse(event) {
     event.preventDefault();
     this.props.actions.saveCourse(this.state.course);
+    this.context.router.push('/courses');
   }
 
   render() {
@@ -47,6 +48,11 @@ ManageCoursePage.propTypes = {
   course: PropTypes.object.isRequired,
   authors: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired
+};
+
+// Pull in the React Router context so router is available in this.context.router
+ManageCoursePage.contextTypes = {
+  router: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps){
