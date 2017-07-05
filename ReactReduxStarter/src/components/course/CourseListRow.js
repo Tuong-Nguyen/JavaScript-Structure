2425e805e8c68330aeab0024c186aba75cbb2644
Le Ngoc Thao <lnthao@tma.com.vn>
2017-07-05 17:29:14 +0700

    Dispatch create and update action

--- ReactReduxStarter/src/components/course/CourseListRow.js
+++ ReactReduxStarter/src/components/course/CourseListRow.js
@@ -9,7 +9,7 @@ const CourseListRow = ({course}) => {
       <tr>
         <td><a href={course.watchHref} target="_blank">Watch</a> </td>
         <td><Link to={'/course/'+ course.id}>{course.title}</Link></td>
-        <td>{course.author}</td>
+        <td>{course.authorId}</td>
         <td>{course.category}</td>
         <td>{course.length}</td>
       </tr>
--- ReactReduxStarter/src/components/course/CoursesPage.js
+++ ReactReduxStarter/src/components/course/CoursesPage.js
@@ -6,10 +6,16 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
+import {browserHistory} from 'react-router';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
+  }
+
+  redirectToAddCoursePage(){
+    browserHistory.push('/course');
   }
 
   render() {
@@ -17,6 +23,10 @@ class CoursesPage extends React.Component {
     return (
       <div>
         <h1>Courses</h1>
+        <input type="submit"
+               value="Add Course"
+               className="btn btn-primary"
+               onClick={this.redirectToAddCoursePage}/>
         <CourseList courses={courses}/>
       </div>
     );
