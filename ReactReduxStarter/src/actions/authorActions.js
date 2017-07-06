c950081b265f819dc1a56fd60f2f435e7a2a2c8e
lnthao <lnthao@tma.com.vn>
2017-07-06 10:47:16 +0700

    dispatch beginAjaxCall action in loadCourses and loadAuthors

--- ReactReduxStarter/src/actions/authorActions.js
+++ ReactReduxStarter/src/actions/authorActions.js
@@ -3,6 +3,7 @@
  */
 import AuthorApi from '../api/mockAuthorApi';
 import * as types from './actionTypes';
+import {beginAjaxCall} from './ajaxStatusActions';
 
 export function loadAuthorsSuccess(authors){
   return {type: types.LOAD_AUTHORS_SUCCESS, authors};
@@ -10,6 +11,7 @@ export function loadAuthorsSuccess(authors){
 
 export function loadAuthors(){
   return dispatch => {
+    dispatch(beginAjaxCall());
     return AuthorApi.getAllAuthors().then(authors => {
       dispatch(loadAuthorsSuccess(authors));
     }).catch(error => {
--- ReactReduxStarter/src/actions/courseActions.js
+++ ReactReduxStarter/src/actions/courseActions.js
@@ -3,6 +3,7 @@
  */
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
+import {beginAjaxCall} from './ajaxStatusActions';
 
 export function loadCoursesSuccess(courses){
   return {type: types.LOAD_COURSES_SUCCESS, courses};
@@ -18,6 +19,7 @@ export function updateCourseSuccess(course){
 
 export function loadCourses(){
   return function(dispatch) {
+    dispatch(beginAjaxCall());
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
     }).catch(error=>{
