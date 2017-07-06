c2fe9d7d8c0497b389e749ebd9fab5a0fe15315d
lnthao <lnthao@tma.com.vn>
2017-07-06 11:01:53 +0700

    Add loading component to header and display it only when ajax call is in progress

--- ReactReduxStarter/src/components/App.js
+++ ReactReduxStarter/src/components/App.js
@@ -3,12 +3,13 @@
  */
 import React, {PropTypes} from 'react';
 import Header from './common/Header';
+import {connect} from 'react-redux';
 
 class App extends React.Component{
   render() {
     return (
       <div className="container-fluild">
-        <Header/>
+        <Header loading={this.props.loading}/>
         {this.props.children}
       </div>
     );
@@ -17,4 +18,10 @@ class App extends React.Component{
 App.propTypes = {
   children: PropTypes.object.isRequired
 };
-export default App;
+
+function mapStateToProps(state, ownProps) {
+  return {
+    loading: state.ajaxCallsInProgress > 0
+  };
+}
+export default connect(mapStateToProps)(App);
