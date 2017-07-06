c0fbc1949e6a0e297266a64c420e9d6f7dbfd16c
lnthao <lnthao@tma.com.vn>
2017-07-06 10:38:39 +0700

    Add ajax status reducer

--- ReactReduxStarter/src/reducers/index.js
+++ ReactReduxStarter/src/reducers/index.js
@@ -4,9 +4,10 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
 import authors from './authorReducer';
+import ajaxCallsInProgress from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({
-  courses, authors
+  courses, authors, ajaxCallsInProgress
 });
 
 export default rootReducer;
