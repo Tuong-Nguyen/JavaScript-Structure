0996b86c0d61252caaab5ee39c5f0e4109d8ba76
Le Ngoc Thao <lnthao@tma.com.vn>
2017-07-06 17:51:10 +0700

    Add test for submit button value in 2 case: course is saving and is not saving

--- ReactReduxStarter/src/components/course/CourseForm.test.js
+++ ReactReduxStarter/src/components/course/CourseForm.test.js
@@ -6,9 +6,9 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import CourseForm from './CourseForm';
 
-function setup(){
+function setup(saving){
   let props = {
-    course: {}, saving: false, errors: {},
+    course: {}, saving: saving, errors: {},
     onSave: () => {},
     onChange: () => {}
   };
@@ -30,5 +30,17 @@ describe('CourseForm via React Test Utils', () => {
     expect(output.type).toBe('form');
     let [h1] = output.props.children;
     expect(h1.type).toBe('h1');
+  });
+
+  it('save button is labeled "Save" when not saving', () => {
+    const {output} = setup(false);
+    const submitButton = output.props.children[5];
+    expect(submitButton.props.value).toBe('Save');
+  });
+
+  it('save button is labeled "Saving..." when saving', () => {
+    const {output} = setup(true);
+    const submitButton = output.props.children[5];
+    expect(submitButton.props.value).toBe('Saving...');
   });
 });
