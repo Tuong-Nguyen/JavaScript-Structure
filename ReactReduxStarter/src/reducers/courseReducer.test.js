e705ab8f8ed8d46ef7a04f30b93ce00fe8e6b36a
Le Ngoc Thao <lnthao@tma.com.vn>
2017-07-07 15:37:39 +0700

    Add test case for course reducer when updating course

--- ReactReduxStarter/src/reducers/courseReducer.test.js
+++ ReactReduxStarter/src/reducers/courseReducer.test.js
@@ -19,4 +19,22 @@ describe('Course Reducer', () => {
     expect(newState[1].title).toEqual('B');
     expect(newState[2].title).toEqual('C');
   });
+  it('should update course when passed UPDATE_COURSE_SUCCESS', () => {
+    // arrange
+    const initialState = [
+      {id: 'A', title: 'A'},
+      {id: 'B', title: 'B'},
+      {id: 'C', title: 'C'}
+    ];
+    const course = {id: 'B', title: 'New title'};
+    const action = actions.updateCourseSuccess(course);
+    // act
+    const newState = courseReducer(initialState, action);
+    const updatedState = newState.find(a => a.id == course.id);
+    const untouchedState = newState.find(a => a.id == 'A');
+    // assert
+    expect(updatedState.title).toEqual('New title');
+    expect(untouchedState.title).toEqual('A');
+    expect(newState.length).toEqual(3);
+  });
 });
