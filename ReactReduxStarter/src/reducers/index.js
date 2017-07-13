dde7da3cd525a87d2be04487a69f5999ea5d2f08
Le Ngoc Thao <lnthao@tma.com.vn>
2017-07-13 11:51:02 +0700

    Update combineReducers parameters to make composition reducers clear

--- ReactReduxStarter/src/reducers/index.js
+++ ReactReduxStarter/src/reducers/index.js
@@ -2,12 +2,14 @@
  * Created by lnthao on 7/4/2017.
  */
 import {combineReducers} from 'redux';
-import courses from './courseReducer';
-import authors from './authorReducer';
+import courseReducer from './courseReducer';
+import authorReducer from './authorReducer';
 import ajaxCallsInProgress from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({
-  courses, authors, ajaxCallsInProgress
+  courses: courseReducer,
+  authors: authorReducer,
+  ajaxCallsInProgress
 });
 
 export default rootReducer;
