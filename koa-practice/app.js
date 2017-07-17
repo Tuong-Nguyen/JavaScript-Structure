ab873f3baaac023097ccfd3b2eb16f8c37431f91
Le Ngoc Thao <lnthao@tma.com.vn>
2017-07-17 15:28:05 +0700

    uncompleted work

---
+++ koa-practice/app.js
@@ -0,0 +1,42 @@
+/**
+ * Created by lnthao on 7/13/2017.
+ */
+const Koa = require('koa');
+const app = module.exports = new Koa();
+
+const bodyParser = require('koa-bodyparser');
+const router = require("koa-router")();
+const mongo = require('koa-mongo');
+const nconf = require('nconf');
+
+nconf.file({file: `${__dirname}/env.json`});
+
+app.use(mongo({
+    host: nconf.get('database:host'),
+    port: nconf.get('database:port'),
+    db: nconf.get('database:db')
+}));
+
+router.post('/user', async (ctx, next) => {
+    let user = ctx.request.body;
+    if (!user.name) {
+        ctx.throw(400, "name required");
+    }
+    const inserted = await ctx.mongo.collection('users').insert(user);
+    ctx.response.set('location', '/user/' + inserted.ops[0]._id.toString());
+    ctx.response.status = 200;
+    next();
+});
+
+router.get('/user/:id', async(ctx, next) => {
+    const id = ctx.params.id;
+    const user = await ctx.mongo.collection('users').findOne({_id: id});
+    ctx.response.body = user;
+    ctx.response.status = 200;
+    next();
+});
+
+app.use(bodyParser());
+app.use(router.routes());
+
+app.listen(3000);
---
+++ koa-practice/test.js
@@ -0,0 +1,60 @@
+/**
+ * Created by lnthao on 7/14/2017.
+ */
+const nconf = require('nconf');
+nconf.file({file: `${__dirname}/env.test.json`});
+
+var app = require("./app.js");
+var request = require("supertest").agent(app.listen());
+var migrations = require("./migrations.js");
+const mm = require("mongodb-migrations");
+
+describe("Simple User Http Crud API", () => {
+    var a_user = {name: "Marcus", age: 42, height: 1.96};
+   it("adds new users", (done) => {
+       request
+           .post("/user")
+           .send(a_user)
+           .expect("location", /^\/user\/[0-9a-fA-F]{24}$/)
+           .expect(200, done);
+   });
+
+   it("fails with validation error for users without name", (done) => {
+      delete a_user.name;
+      request
+          .post("/user")
+          .send(a_user)
+          .expect("name required")
+          .expect(400, done);
+   });
+
+   describe("Migrations test", ()=> {
+       const config = {
+           host: nconf.get('database:host'),
+           port: nconf.get('database:port'),
+           db: nconf.get('database:db')
+       };
+       const migrator = new mm.Migrator(config);
+
+       before((done) => {
+           migrator.add(migrations);
+           migrator.migrate(done);
+       });
+
+       after((done) => {
+           migrator.rollback(done);
+       });
+
+       it("get existing user by id", (done) => {
+           const existedUserId = migrations.existingUserIds[0];
+           const url = '/user/' + existedUserId;
+           request
+               .get(url)
+               .set("Accept", "application/json")
+               .expect("Content-type", /json/)
+               .expect(/Marcus/)
+               .expect(/1.96/)
+               .expect(200, done);
+       });
+   });
+});
