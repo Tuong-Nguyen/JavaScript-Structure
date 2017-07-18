b9ce9822a21ad6c56343fa75a4d15a26070dc1f1
Le Ngoc Thao <lnthao@tma.com.vn>
2017-07-18 14:58:14 +0700

    Add app layout and update home view

--- koa-practice/routes/homeRoutes.js
+++ koa-practice/routes/homeRoutes.js
@@ -2,7 +2,7 @@
  * Created by lnthao on 7/18/2017.
  */
 const showHome = async(ctx, next)=>{
-    await ctx.render("home.pug");
+    await ctx.render("home.pug", {questions:[]});
 };
 
 module.exports = {showHome};
