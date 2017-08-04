3ea026e6eedd3443ff1e6154d38cad63a29f423e
Thao Le <thaoln87@gmail.com>
2017-08-04 15:12:07 +0700

    Add Company -> Users relationship

--- graphql-tutorial/schema/schema.js
+++ graphql-tutorial/schema/schema.js
@@ -1,13 +1,38 @@
-import {GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema} from 'graphql';
+import {GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema, GraphQLList} from 'graphql';
 import _ from 'lodash';
 import axios from 'axios';
 
-const UserType = new GraphQLObjectType({
+let UserType;
+
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: () => ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        description: {type: GraphQLString},
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+                    .then(response => response.data)
+            }
+        }
+    })
+});
+
+UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: {type: GraphQLString},
         firstName: {type: GraphQLString},
-        age: {type: GraphQLInt}
+        age: {type: GraphQLInt},
+        company: {
+            type: CompanyType,
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                    .then(response => response.data);
+            }
+        }
     }
 });
 
@@ -21,6 +46,14 @@ const RootQuery = new GraphQLObjectType({
                 return axios.get("http://localhost:3000/users")
                     .then(response => response.data)
                     .then(users => _.find(users, {id: args.id}));
+            }
+        },
+        company: {
+            type: CompanyType,
+            args: {id: {type: GraphQLString}},
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${args.id}`)
+                    .then(response => response.data)
             }
         }
     }
