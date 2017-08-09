5dd4a235d8592e12e1d9426474e723c6862b1c22
Le Ngoc Thao <lnthao@tma.com.vn>
2017-08-09 14:52:39 +0700

    Add demo for presets

---
+++ ReactReduxStarter/src/stories/common/DivWithPresets.js
@@ -0,0 +1,71 @@
+/**
+ * Created by lnthao on 8/9/2017.
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
+import {Motion, spring, presets} from 'react-motion';
+import Grid from 'material-ui/Grid';
+
+const styleSheet = createStyleSheet(theme => ({
+  div: {
+    minWidth: 275,
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: 3,
+    border: 0,
+    color: 'white',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .30)',
+    textAlign: 'center'
+  }
+}));
+
+const DivWithPresets = ({classes}) => {
+  return (
+    <div>
+      <Grid container spacing={16}>
+        <Grid item xs={3}>
+          <Motion defaultStyle={{h:0}} style={{h: spring(500)}}>
+            {
+              ({h}) => <div className={classes.div} style={{height: h}}>
+                {'Default is noWobble'}
+              </div>
+            }
+          </Motion>
+        </Grid>
+        <Grid item xs={3}>
+          <Motion defaultStyle={{h:0}} style={{h: spring(500, presets.gentle)}}>
+            {
+              ({h}) => <div className={classes.div} style={{height: h}}>
+                {'Gentle'}
+              </div>
+            }
+          </Motion>
+        </Grid>
+        <Grid item xs={3}>
+          <Motion defaultStyle={{h:0}} style={{h: spring(500, presets.wobbly)}}>
+            {
+              ({h}) => <div className={classes.div} style={{height: h}}>
+                {'Wobbly'}
+              </div>
+            }
+          </Motion>
+        </Grid>
+        <Grid item xs={3}>
+          <Motion defaultStyle={{h:0}} style={{h: spring(500, presets.stiff)}}>
+            {
+              ({h}) => <div className={classes.div} style={{height: h}}>
+                {'Stiff'}
+              </div>
+            }
+          </Motion>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+DivWithPresets.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styleSheet)(DivWithPresets);
--- ReactReduxStarter/src/stories/materialUI/ReactMotion.story.js
+++ ReactReduxStarter/src/stories/materialUI/ReactMotion.story.js
@@ -5,8 +5,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import ListAndButtonWithMotion from './../../components/common/ListAndButtonWithMotion';
+import DivWithPresets from './../common/DivWithPresets';
 
 storiesOf("React Motion", module)
   .add('List and Button with motion', () => (
     <ListAndButtonWithMotion />
-))
+  ))
+  .add('Div with presets', () => (
+    <DivWithPresets />
+  ));
