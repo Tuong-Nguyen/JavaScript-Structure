ac450d2b1951a4f1d18beb95cc109cd3b905c3f9
Le Ngoc Thao <lnthao@tma.com.vn>
2017-08-09 09:21:40 +0700

    Make donut to dumb component

--- ReactReduxStarter/src/stories/common/Donut.js
+++ ReactReduxStarter/src/stories/common/Donut.js
@@ -7,58 +7,30 @@ import { withStyles, withTheme, createStyleSheet, createMuiTheme } from 'materia
 import Grid from 'material-ui/Grid';
 import {Motion, spring} from 'react-motion';
 
-
-// 1. We define the styles.
-const styleSheet = createStyleSheet(theme => ({
-  root: {
-    color: 'inherit'
-  },
-  primary: {
-    color: theme.palette.primary[500]
-  }
-}));
-
-class Donut extends Component {
-  state = {
-    percent: 0,
-    colorDefault: 'grey',
-    color: 'blue'
-  };
-
-  componentDidMount(){
-    const colorPalette = this.props.theme.palette;
-    const donutColor = this.props.percent > 25 ? (this.props.percent > 50 ? colorPalette.error[400] : colorPalette.accent[400])
-                                                : colorPalette.primary[400];
-    console.log('componentDidMount ---- ' + this.props.percent);
-    this.setState((state) => ({
-      percent: this.props.percent,
-      colorDefault: colorPalette.grey[200],
-      color: donutColor
-    }))
-  }
-
-  render() {
-    console.log('render --- ' + this.state.percent);
-    return (
-      <Grid item xs={6} sm={3}>
-        <Motion style={{slicePercent: spring(this.state.percent, {stiffness: 70, damping: 30})}}>
-          {
-            ({slicePercent}) =>
-              <svg width="100%" height="100%" viewBox="0 0 42 42" class="donut">
-                <circle class="donut-ring" cx="21" cy="21" r="15" fill="transparent" stroke={this.state.colorDefault}
-                        strokeWidth="5"></circle>
-                <circle class="donut-segment" cx="21" cy="21" r="15" fill="transparent"
-                        stroke={this.state.color} strokeWidth="5"
-                        strokeDasharray={slicePercent + ' ' + (100 - slicePercent)}
-                        strokeDashoffset="25"></circle>
-                <text x="50%" y="55%" textAnchor="middle" fontSize={8} fill="grey">{(Math.round(slicePercent))}%</text>
-              </svg>
-          }
-        </Motion>
-      </Grid>
-    );
-  }
-}
+const Donut = ({percent, theme}) => {
+  const colorPalette = theme.palette;
+  const donutColor = percent > 25 ? (percent > 50 ? colorPalette.error[400] : colorPalette.accent[400])
+    : colorPalette.primary[400];
+  console.log('render --- ' + percent);
+  return (
+    <Grid item xs={6} sm={3}>
+      <Motion defaultStyle={{slicePercent: 0}} style={{slicePercent: spring(percent, {stiffness: 70, damping: 30})}}>
+        {
+          ({slicePercent}) =>
+            <svg width="100%" height="100%" viewBox="0 0 42 42" class="donut">
+              <circle class="donut-ring" cx="21" cy="21" r="15" fill="transparent" stroke={colorPalette.grey[200]}
+                      strokeWidth="5"></circle>
+              <circle class="donut-segment" cx="21" cy="21" r="15" fill="transparent"
+                      stroke={donutColor} strokeWidth="5"
+                      strokeDasharray={slicePercent + ' ' + (100 - slicePercent)}
+                      strokeDashoffset="25"></circle>
+              <text x="50%" y="55%" textAnchor="middle" fontSize={8} fill="grey">{(Math.round(slicePercent))}%</text>
+            </svg>
+        }
+      </Motion>
+    </Grid>
+  );
+};
 
 Donut.defaultProps = {
   percent: 50
