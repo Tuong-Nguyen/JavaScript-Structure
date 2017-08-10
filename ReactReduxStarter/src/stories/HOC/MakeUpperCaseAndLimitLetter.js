ea7839b9e1b54b0f35acdd16998b4e6a2ef1c7e6
Le Ngoc Thao <lnthao@tma.com.vn>
2017-08-10 16:42:45 +0700

    Change HoC to stateless

--- ReactReduxStarter/src/stories/HOC/MakeUpperCaseAndLimitLetter.js
+++ ReactReduxStarter/src/stories/HOC/MakeUpperCaseAndLimitLetter.js
@@ -4,17 +4,15 @@
 import React from 'react';
 
 export const makeUpperCaseAndLimitLetter = (limitLetter) => (WrappedComponent) => {
-  return class MakeUpperCaseAndLimitLetter extends React.Component {
-    render() {
-      let title = this.props.title;
-      if (title.length > limitLetter) {
-        title = title.substring(0, limitLetter) + '...';
-      }
-      const props = Object.assign({}, this.props, {
-        title: title.toUpperCase()
-      });
-
-      return <WrappedComponent { ...props } />
+  return (props) => {
+    let title = props.title;
+    if (title.length > limitLetter) {
+      title = title.substring(0, limitLetter) + '...';
     }
+    const childProps = Object.assign({}, props, {
+      title: title.toUpperCase()
+    });
+
+    return <WrappedComponent { ...childProps } />
   };
 };
