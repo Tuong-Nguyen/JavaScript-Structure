6758b6886c023480c77f235a067ed59274c1d0e3
nguyenkim42 <nkim@tma.com.vn>
2017-08-11 18:36:11 +0700

    Begin react-routerV4

---
+++ react-router/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import './App.css';
+
+const Links = () => (
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to={{pathname: '/about'}}>About</Link>
+        <Link replace to="/contact">Contact</Link>
+    </nav>
+)
+
+const App = () => (
+    <Router>
+        <div>
+            <Links />
+            <Route exact path="/" render={() => <h1>Home</h1>} />
+            <Route path="/about" render={() => <h1>About</h1>} />
+            <Route path="/contact" render={() => <h1>Contact</h1>} />
+        </div>
+    </Router>
+);
+
+export default App
