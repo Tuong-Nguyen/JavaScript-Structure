a2e8c8efe5490b39b83f0bb2472453bd93b8062e
Le Ngoc Thao <lnthao@tma.com.vn>
2017-08-14 15:24:47 +0700

    Add test cases for shallow dive or not

--- ReactReduxStarter/src/stories/common/CandyButton.test.js
+++ ReactReduxStarter/src/stories/common/CandyButton.test.js
@@ -9,15 +9,21 @@ import CandyButton from './CandyButton';
 import AddAlarm from 'material-ui-icons/AddAlarm';
 
 describe('CandyButton test', () => {
-let shallow;
-
-  beforeAll(() => {
-   shallow = createShallow({dive: true});
+  test('CandyButton has alarm icon', () => {
+    const shallow = createShallow({dive: true});
+    const wrapper = shallow(<CandyButton/>);
+    expect(wrapper.find(AddAlarm).length).toBe(1);
   });
 
-  test('CandyButton has alarm icon', () => {
-    const wrapper = shallow(<CandyButton />);
-    console.log(wrapper.props());
-    expect(wrapper.find(AddAlarm).length).toBe(1);
+  test("Dive shallow - wrapper's current node name is withStyles(Button)", () => {
+    const shallow = createShallow({dive: true});
+    const wrapper = shallow(<CandyButton/>);
+    expect(wrapper.name()).toEqual('withStyles(Button)');
+  });
+
+  test("Non-dive shallow - wrapper's current node name is CandyButton", () => {
+    const shallow = createShallow({dive: false});
+    const wrapper = shallow(<CandyButton/>);
+    expect(wrapper.name()).toEqual('CandyButton');
   });
 });
