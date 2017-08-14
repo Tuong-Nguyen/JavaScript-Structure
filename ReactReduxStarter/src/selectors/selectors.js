68f2107c79b61a6211d97bb4895d06150885337c
ntthanh1 <ntthanh1@tma.com.vn>
2017-08-14 18:18:15 +0700

    add selector for mapStateToProps

--- ReactReduxStarter/src/selectors/selectors.js
+++ ReactReduxStarter/src/selectors/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from 'reselect';
+
 export function authorsFormattedForDropdown(authors) {
   return authors.map(author => {
     return {
@@ -6,3 +8,17 @@ export function authorsFormattedForDropdown(authors) {
     };
   });
 }
+
+export const setStateOfCourses = createSelector(
+  [courses => courses],
+  (courses) => {
+    return (courses == null || courses == []) ? {
+      id: "clean-code",
+      title: "Clean Code: Writing Code for Humans",
+      watchHref: "http://www.pluralsight.com/courses/writing-clean-code-humans",
+      authorId: "cory-house",
+      length: "3:10",
+      category: "Software Practices"
+    } : courses;
+  }
+);
--- ReactReduxStarter/src/selectors/selectors.test.js
+++ ReactReduxStarter/src/selectors/selectors.test.js
@@ -1,5 +1,48 @@
 import expect from 'expect';
-import { authorsFormattedForDropdown } from "./selectors";
+import { authorsFormattedForDropdown, setStateOfCourses } from "./selectors";
+
+const coursesList = [
+  {
+    id: "react-flux-building-applications",
+    title: "Building Applications in React and Flux",
+    watchHref: "http://www.pluralsight.com/courses/react-flux-building-applications",
+    authorId: "cory-house",
+    length: "5:08",
+    category: "JavaScript"
+  },
+  {
+    id: "clean-code",
+    title: "Clean Code: Writing Code for Humans",
+    watchHref: "http://www.pluralsight.com/courses/writing-clean-code-humans",
+    authorId: "cory-house",
+    length: "3:10",
+    category: "Software Practices"
+  },
+  {
+    id: "architecture",
+    title: "Architecting Applications for the Real World",
+    watchHref: "http://www.pluralsight.com/courses/architecting-applications-dotnet",
+    authorId: "cory-house",
+    length: "2:52",
+    category: "Software Architecture"
+  },
+  {
+    id: "career-reboot-for-developer-mind",
+    title: "Becoming an Outlier: Reprogramming the Developer Mind",
+    watchHref: "http://www.pluralsight.com/courses/career-reboot-for-developer-mind",
+    authorId: "cory-house",
+    length: "2:30",
+    category: "Career"
+  },
+  {
+    id: "web-components-shadow-dom",
+    title: "Web Component Fundamentals",
+    watchHref: "http://www.pluralsight.com/courses/web-components-shadow-dom",
+    authorId: "cory-house",
+    length: "5:10",
+    category: "HTML5"
+  }
+];
 
 describe('Author Selectors', () => {
   describe('authorsFormattedForDropdown', () => {
@@ -23,6 +66,23 @@ describe('Author Selectors', () => {
       ];
 
       expect(authorsFormattedForDropdown(authors)).toEqual(expected);
+    });
+  });
+});
+
+
+
+describe('Courses Selectors', () => {
+  describe('mapStateToProps from state of courses', () => {
+    it('should return courses data', () => {
+      const courses =  coursesList;
+      expect(setStateOfCourses(courses)).toEqual(courses);
+    });
+
+    it('should return courses data is default', () => {
+      const courses = null;
+      let a = setStateOfCourses(courses);
+      expect(setStateOfCourses(courses)).toEqual(coursesList[1]);
     });
   });
 });
